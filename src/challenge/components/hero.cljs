2fbbc379f6186f9437422d1020e32fb4d37e0fdc
Diego Comesaña <diego.comesana@gmail.com>
2021-10-10 20:31:57 -0300

    wip

--- src/challenge/components/hero.cljs
+++ src/challenge/components/hero.cljs
@@ -6,12 +6,12 @@
 
 (defclass style
   []
-  {:color :white :padding "20px" :whidth "100%" :background-color :green})
+  {:color :black :padding "20px" :whidth "100%" :background-color :magenta})
 
 
 (defn component
   [{:keys [image link]}]
-  [:div {:class (style)}
+  [:div {:class (style) :style {:background (str "no-repeat center url(" (:src image) ")")}}
    [:h2 "HERO"]
    [:p {:key "image"} "image: " (:src image)]
    [:p {:key "link"} "link: " (:href link)]])
